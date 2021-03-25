46421ff239dbcaf6c086b081de18f055b8806130
Juliaashr <81264191+Juliaashr@users.noreply.github.com>
2021-03-25 23:47:45 +0300

    Update Program.cs

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -10,46 +10,42 @@ namespace Shop
     {
         static void Main(string[] args)
         {
+            string[] names = new string[10] {"Светлана","Валентин","Никита","Юлия","Анна",
+                                             "Алина","Вероника","Степан","Григорий","Петр" };
+
             Product<string>[] offeredProducts = {new Product<string>("продукты"), new Product<string>("товары для детей"),
                                                  new Product<string>("бытовые товары"), new Product<string>("товары для авто"),
                                                  new Product<string>("зоотовары"), new Product<string>("косметика"),
-                                                 new Product<string>("лекарства"), new Product<string>("одежда и обувь")};
+                                                 new Product<string>("лекарства"), new Product<string>("одежда и обувь")
+            };
 
             Console.WriteLine("список предлагаемых товаров:");
 
             for (int i = 0; i < offeredProducts.Length; i++)
-                Console.WriteLine(offeredProducts[i]);
+                Console.WriteLine($"{i+1}. {offeredProducts[i]}") ;
 
+ 
             IDictionary<Guid, Customer<string, string>> customers = new Dictionary<Guid, Customer<string, string>>(10);
 
-            var c1 = new Customer<string, string>("Антон", new Carts<string>(3, offeredProducts));
-            var c2 = new Customer<string, string>("Кирилл", new Carts<string>(4, offeredProducts));
-            var c3 = new Customer<string, string>("Анна", new Carts<string>(3, offeredProducts));
-            var c4 = new Customer<string, string>("Юлия", new Carts<string>(5, offeredProducts));
-            var c5 = new Customer<string, string>("Павел", new Carts<string>(1, offeredProducts));
-            var c6 = new Customer<string, string>("Алина", new Carts<string>(3, offeredProducts));
-            var c7 = new Customer<string, string>("Константин", new Carts<string>(4, offeredProducts));
-            var c8 = new Customer<string, string>("Дарья", new Carts<string>(5, offeredProducts));
-            var c9 = new Customer<string, string>("Светлана", new Carts<string>(3, offeredProducts));
-            var c10 = new Customer<string, string>("Валентин", new Carts<string>(2, offeredProducts));
+            for (int j = 0; j < names.Length; j++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Введите количесво желаемых товаров для покупателя {names[j]}:");
 
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    var c = new Customer<string, string>(names[j], new Carts<string>(int.Parse(Console.ReadLine()), offeredProducts));
 
-            customers[c1.Code] = c1;
-            customers[c2.Code] = c2;
-            customers[c3.Code] = c3;
-            customers[c4.Code] = c4;
-            customers[c5.Code] = c5;
-            customers[c6.Code] = c6;
-            customers[c7.Code] = c7;
-            customers[c8.Code] = c8;
-            customers[c9.Code] = c9;
-            customers[c10.Code] = c10;
-
+                    customers[c.Code] = c;
+                }
+                catch(FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }  
+            }
 
             Help.Service(customers);
-
-
-
 
             Console.ReadKey();
         }
