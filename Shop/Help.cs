a8b89f2e4d8d63ee62d1b13a4d8e3a04bc27bead
Juliaashr <81264191+Juliaashr@users.noreply.github.com>
2021-03-25 23:49:43 +0300

    Update Help.cs

--- Shop/Help.cs
+++ Shop/Help.cs
@@ -10,46 +10,48 @@ namespace Shop
     {
         public static void Service(IDictionary<Guid, Customer<string, string>> customers)
         {
-            while (true)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Red;
 
                 Console.WriteLine("Введите 1, чтобы добавить покупателя; введите 2, чтобы просмотреть список покупателей; введите 3, чтобы начать поиск; 4 - чтобы выйти:");
 
                 string helpVal = Console.ReadLine();
 
-                if (helpVal == "1")
+                switch(helpVal)
                 {
-                    Console.WriteLine("Введите имя покупателя и коичество желаемых товаров:");
+                    case "1":
+                        Console.WriteLine("Введите имя покупателя и количество желаемых товаров:");
 
-                    Console.ForegroundColor = ConsoleColor.Green;
-
-                    var newC = new Customer<string, string>(Console.ReadLine(), new Carts<string>(int.Parse(Console.ReadLine())));
-                    customers.Add(newC.Code, newC);
-                }
-
-                if (helpVal == "2")
-                    foreach (KeyValuePair<Guid, Customer<string, string>> item in customers)
-                    {
                         Console.ForegroundColor = ConsoleColor.Green;
 
-                        Console.WriteLine($"{item.Key} : {item.Value} - корзина:");
-                        item.Value.ShowCart();
-                    }
+                        var newC = new Customer<string, string>(Console.ReadLine(), new Carts<string>(int.Parse(Console.ReadLine())));
+                        customers.Add(newC.Code, newC);
+                        Help.Service(customers);
 
-                if (helpVal == "3")
-                {
+                        break;
+                    case "2":
+                        foreach (KeyValuePair<Guid, Customer<string, string>> item in customers)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
 
-                    Console.WriteLine("Введите уникальный код для поиска покупателя: ");
+                            Console.WriteLine($"{item.Key} : {item.Value} - корзина:");
+                            item.Value.ShowCart();
+                        }
+                        Help.Service(customers);
+                        break;
+                    case "3":
+                        Console.WriteLine("Введите уникальный код для поиска покупателя: ");
 
-                    Console.ForegroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Green;
 
-                    Find.TryGetCartOrder(customers, Guid.Parse(Console.ReadLine()));
+                        Find.TryGetCartOrder(customers, Guid.Parse(Console.ReadLine()));
+
+                        Help.Service(customers);
+                        break;
+                    case "4":
+                        Console.WriteLine("Выполнен выход!");
+                        break;
+
                 }
-
-                if (helpVal == "4")
-                    break;
-            }
         }
     }
 }
